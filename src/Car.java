6c6a997289c0192f2d11c2b4dc8dcf88e4a74acd
TienPhuc02 <dtp2352002@gmail.com>
2024-02-15 10:14:10 +0700

    introduction classes, using getter ,method

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,32 @@
 public class Car {
-    private String make;
-    private String model;
-    private String color;
-    private String doors;
-    private boolean convertible;
-    public void describeCar(){
-        System.out.println(make + model + color + doors +  (convertible ? "convertible ":""));
+    private String make="Tesla ";
+    private String model ="Model X ";
+    private String color = "Gray "; // private nen Main.java khong the dat gia tri
+    private int doors=2;
+    private boolean convertible=true;
+
+    public String getModel() {
+        return model;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void describeCar(){
+        System.out.println(make + model + color +" " + doors+" " +  (convertible ? "convertible ":""));
+    }
+    // làm sao để làm mới casc giá trị thuộc tính mỗi khi tạo mới 1 ô tô? -> để public các thuộc tính trong class
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        //oop: trang thai va hanh vi
+        Car car =new Car();
+//        car.make="Porche";//error vì không thể đặt giá trị các thuộc tính vì các thuộc tính trong class Car đang private
+//        System.out.println(car.make);// error vì không thể truy cập các giá trị thuộc tính trong class vì  các thuộc tính đang để private
+        System.out.println("make = " + car.getMake()); // lấy được giá trị private trong class vì có hàm trả về giá trị đó để public
+        car.describeCar();
+
     }
 }
