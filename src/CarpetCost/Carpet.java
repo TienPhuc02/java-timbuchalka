5235acf68b416d043b12c833087debbec88612d9
TienPhuc02 <dtp2352002@gmail.com>
2024-02-16 11:08:15 +0700

    carpet cost calculator

---
+++ src/CarpetCost/Carpet.java
@@ -0,0 +1,21 @@
+package CarpetCost;
+
+public class Carpet {
+    private double cost;
+
+    public Carpet(double cost) {
+        if(cost<0){
+            this.cost=0;
+        }
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        if(cost<0){
+            return cost=0;
+        }else{
+            return cost;
+        }
+
+    }
+}
