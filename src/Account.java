061d4b5d4ac76e0c7bf74845229aa8efd87fc814
TienPhuc02 <dtp2352002@gmail.com>
2024-02-16 09:00:49 +0700

    constructor part2

--- src/Account.java
+++ src/Account.java
@@ -8,16 +8,25 @@ public class Account {
     private String email;
     private String soDienThoai;
     public Account(){
+        this(1234567891,1230000,"Phuc this","dtp2352002@gmail.com","0912928210");//parameter của hàm this là như parameter của class Account
         System.out.println("Constructor empty called.");
     }
     public Account(int soTaiKhoan,int soDu,String tenKhachHang,String email,String soDienThoai){
         System.out.println("Account Constructor with parameters called.");
-        this.soTaiKhoan=soTaiKhoan;
+        this.soTaiKhoan=soTaiKhoan;// bằng với hàm setter
         this.soDu=soDu;
         this.tenKhachHang=tenKhachHang;
         this.email=email;
         this.soDienThoai=soDienThoai;
     }
+
+    public Account(String tenKhachHang, String email, String soDienThoai) {
+        this(987654321,123456,tenKhachHang,email,soDienThoai);
+//        this.tenKhachHang = tenKhachHang;
+//        this.email = email;
+//        this.soDienThoai = soDienThoai;
+    }
+
     public  void guiTienVaoTaiKhoan(int tienGuiVao){
         this.soDu+=tienGuiVao;
         DecimalFormat formatter = new DecimalFormat("###,###,###"); // Định dạng số
