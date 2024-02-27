412a05a2844b0e8383965f09bbb5090dfc072329
TienPhuc02 <dtp2352002@gmail.com>
2024-02-27 11:54:54 +0700

    OOP Master Challege Ex Part2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-    Item coke = new Item("drink", "coke",1.5);
-    coke.printItem();
-    coke.setSize("LARGE");
-    coke.printItem();
+//    Item coke = new Item("drink", "coke",1.5);
+//    coke.printItem();
+//    coke.setSize("LARGE");
+//    coke.printItem();
+//
+//    Item avocado = new Item("Topping","avocado",1.5);
+//    avocado.printItem();
 
-    Item avocado = new Item("Topping","avocado",1.5);
-    avocado.printItem();
+//        Burger burger =new Burger("regular",4.00);
+//        burger.addTopping("BACON","CHEESE","MAYO");
+//        burger.printItem(); // 6.5 = 2.5(topping) + 4(base price)
+
+
+
+//        MealOrder regular =new MealOrder();
+//        regular.addBurgerTopping("BACON","CHEESE","MAYO");
+//        regular.setDrinkSize("LARGE");
+//        regular.printItemizedList();
+
+        MealOrder secondMeal= new MealOrder("turkey","7-up","chili");
+        secondMeal.addBurgerTopping("LETTUCE","CHEESE","MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
     }
+
+
 }
