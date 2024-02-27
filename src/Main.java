48182481a3cc80db8660a086ba004bc0a80eee01
TienPhuc02 <dtp2352002@gmail.com>
2024-02-27 14:54:58 +0700

    OOP master Challenge Ex part3

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,16 @@ public class Main {
 //        regular.setDrinkSize("LARGE");
 //        regular.printItemizedList();
 
-        MealOrder secondMeal= new MealOrder("turkey","7-up","chili");
-        secondMeal.addBurgerTopping("LETTUCE","CHEESE","MAYO");
-        secondMeal.setDrinkSize("SMALL");
-        secondMeal.printItemizedList();
+//        MealOrder secondMeal= new MealOrder("turkey","7-up","chili");
+//        secondMeal.addBurgerTopping("LETTUCE","CHEESE","MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+
+        MealOrder deluxeMeal =new MealOrder("deluxe","7-up","chili");
+        deluxeMeal.addBurgerToppings("AVOCADO","BACON","LETTUCE","CHEESE","MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
     }
 
 
