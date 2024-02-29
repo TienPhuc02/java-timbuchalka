3162b88dcea608d8f608db290ce09075a1392105
TienPhuc02 <dtp2352002@gmail.com>
2024-02-29 20:46:43 +0700

    using java.util.arrays(sort,full,copyof)

--- src/dev/array/Main.java
+++ src/dev/array/Main.java
@@ -1,55 +1,45 @@
 package dev.array;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] myIntArray= new int[10];// khong the thay doi
-//        myIntArray[0]=45.0;//error -> double
-//        myIntArray[1]="1"; //error->string
-         myIntArray[5] =50;
-
-        double[] myDOubleArray=new double[10];
-        myDOubleArray[2]=3.5;
-        System.out.println(myDOubleArray[2]);//3.5
-
-        int[] firstFivePositives =new int[]{1,2,3,4,5};
-        int[] firstFivePositives1={1,2,3,4,5};//cach2
-        System.out.println("first =" +firstFivePositives[0]);
-        int arrayLength = firstFivePositives.length;
-        System.out.println("firstFivePositive length of array "+  arrayLength);
-//        System.out.println(firstFivePositives[arrayLength]);//error
-        String[] names={"Phuc","Nam","hieu"};
-
-        int[] newArray;
-//        newArray = {5,4,3,2,1};//error
-//        newArray= new int[]{5,4,3,2,1};
-        newArray=new int[5];
-        for(int i=0;i<newArray.length;i++){
-            newArray[i]= newArray.length - i;
-        }
-        for(int i =0; i<newArray.length;i++){
-            System.out.println(newArray[i]+ " ");
-        }
-        System.out.println();
-        for (int element:newArray){
-            System.out.println(element + " ");
-        }
-
+        int[] newArray= getRandomArray(10);
+        System.out.println(Arrays.toString(newArray));
+        Arrays.sort(newArray);
         System.out.println(Arrays.toString(newArray));
 
-        Object objectVariable =newArray;
+        int[] secondArray =new int[10];
+        System.out.println(Arrays.toString(secondArray));
+        Arrays.fill(secondArray,5);
+        System.out.println(Arrays.toString(secondArray));
 
-        if(objectVariable instanceof int[]){
-            System.out.println("objectVariable is really an int array");
+
+        int[] thirdArray=getRandomArray(10);
+        System.out.println(Arrays.toString(thirdArray));
+        int[] fourthArray=Arrays.copyOf(thirdArray,thirdArray.length);
+        System.out.println(Arrays.toString(fourthArray));
+
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(fourthArray));
+
+        int[] smallerArray=Arrays.copyOf(thirdArray,5);
+        System.out.println(Arrays.toString(smallerArray));
+
+        int[] largerArray=Arrays.copyOf(thirdArray,15);
+        System.out.println(Arrays.toString(largerArray));
+    }
+    private static int[] getRandomArray(int len){
+        Random random =new Random();
+        int[] intArray=new int[len];
+        for(int i=0;i<len;i++){
+            intArray[i]=random.nextInt(100);
         }
-        Object[] objectArray =new Object[3];
-        objectArray[0] = "hello";
-        objectArray[1]=new StringBuilder("World");
-        objectArray[2]=newArray;
-
+        return intArray;
     }
 
 
