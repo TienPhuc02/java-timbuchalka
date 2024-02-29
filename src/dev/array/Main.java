f589f5214d4afba0db46854236c9b2002e844a3a
TienPhuc02 <dtp2352002@gmail.com>
2024-02-29 21:42:57 +0700

    exercise challenge

--- src/dev/array/Main.java
+++ src/dev/array/Main.java
@@ -6,59 +6,40 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
-        int[] newArray= getRandomArray(10);
-        System.out.println(Arrays.toString(newArray));
-        Arrays.sort(newArray);
-        System.out.println(Arrays.toString(newArray));
-
-        int[] secondArray =new int[10];
-        System.out.println(Arrays.toString(secondArray));
-        Arrays.fill(secondArray,5);
-        System.out.println(Arrays.toString(secondArray));
-
-
-        int[] thirdArray=getRandomArray(10);
-        System.out.println(Arrays.toString(thirdArray));
-        int[] fourthArray=Arrays.copyOf(thirdArray,thirdArray.length);
-        System.out.println(Arrays.toString(fourthArray));
-
-        Arrays.sort(fourthArray);
-        System.out.println(Arrays.toString(thirdArray));
-        System.out.println(Arrays.toString(fourthArray));
-
-        int[] smallerArray=Arrays.copyOf(thirdArray,5);
-        System.out.println(Arrays.toString(smallerArray));
-
-        int[] largerArray=Arrays.copyOf(thirdArray,15);
-        System.out.println(Arrays.toString(largerArray));
+        int[] intArray =getRandomArray(5);
+        System.out.println(Arrays.toString(intArray));
+       int[] sortedResult =sortInteger(intArray);
+        System.out.println(Arrays.toString(sortedResult));
 
 
 
-        String[] sArray ={"Able","Phuc","Zues","Keria","Faker"};
-        Arrays.sort(sArray);
-        System.out.println(Arrays.toString(sArray));
-        if(Arrays.binarySearch(sArray,"Keria")>=0){
-            System.out.println("Found Keria in  the list");
-        }
 
-
-        int[] s1={1,2,3,4,5};
-        int[] s2={1,2,3,4,5};
-        if(Arrays.equals(s1,s2)){
-            System.out.println("Array are equal");
-        }else {
-            System.out.println("Array are not equal");
-        }
     }
-    private static int[] getRandomArray(int len){
+    private  static int[] getRandomArray(int len){
+        int[] randomArray=new int[len];
         Random random =new Random();
-        int[] intArray=new int[len];
         for(int i=0;i<len;i++){
-            intArray[i]=random.nextInt(100);
+            randomArray[i]=random.nextInt(1000);
         }
-        return intArray;
+        return randomArray;
     }
 
+    private static int[] sortInteger(int[] array){
+        int[] sortedArray =Arrays.copyOf(array,array.length);
+        boolean flag=true;
+        int temp;
+        while(flag){
+            flag=false;
+            for(int i=0;i<sortedArray.length-1;i++){
+                if(sortedArray[i]<sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
 
 }
