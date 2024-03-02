3e20ccfc4cebed180809383b3cb4bd8b45a32195
TienPhuc02 <dtp2352002@gmail.com>
2024-03-02 09:01:55 +0700

    challenge small array

--- src/dev/array/Main.java
+++ src/dev/array/Main.java
@@ -1,31 +1,40 @@
 package dev.array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String... args) {
 
-        System.out.println("hello world again");
-        String[] splitStrings="Hello World Again".split(" ");
-        printText(splitStrings);
+    int[] returnedArray =readIntegers();
+        System.out.println(Arrays.toString(returnedArray));
+        int returnMin=findMind(returnedArray);
+        System.out.println("min =" + returnMin);
 
-        System.out.println("_".repeat(20));
-
-        printText("Hello");
-
-        System.out.println("_".repeat(20));
-
-        printText("Hello","World","Again");
-        System.out.println("_".repeat(20));
-
-        printText();
-
-
-        String[] sArray={"Guma","Keria","Feker","Zues","Oner"};
-        System.out.println(String.join(",",sArray));
     }
-    public static void printText(String... textlist){
-        for(String t : textlist){
-            System.out.println(t);
+
+    public static int[] readIntegers() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("_".repeat(20));
+
+        String input =scanner.nextLine();
+        String[] splits=input.split(",");
+        int[] values =new int[splits.length];
+        for(int i=0 ; i<splits.length;i++){
+            values[i]=Integer.parseInt(splits[i].trim());
         }
+        return  values;
+    }
+
+    public static int findMind(int[] array){
+        int min =Integer.MAX_VALUE;
+        for(int el :array){
+            if(el <min){
+                min=el;
+            }
+        }
+        return min;
     }
 }
